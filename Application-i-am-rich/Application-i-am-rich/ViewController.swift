5a363e9cf37fcd78a3e8dbb63071ea3d14494047
QuanDoCoder <dracolucius05032002@gmail.com>
2021-12-16 11:36:00 +0700

    modified

---
+++ Application-i-am-rich/Application-i-am-rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Application-i-am-rich
+//
+//  Created by Quan's Macbook on 02/10/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
